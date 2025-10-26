9854cbd0bced7b3dc94c83ad579874f451671154
charred-70 <129462659+charred-70@users.noreply.github.com>
2025-10-26 06:19:30 -0700

    contact page

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -5,6 +5,12 @@ const Contact = () => {
     <div>
       <h1>Contact Us</h1>
       <p className="Contact">Feel free to email any of us for questions or concerns!</p>
+      <div class='contact-pictures'>
+        <img src='char.png'></img>
+        <img src='cindy.png'></img>
+        <img src='rachel.png'></img>
+        <img src='tiff.png'></img>
+      </div>
     </div>
   )
 }
